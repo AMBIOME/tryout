fcee1b6e98cd13a8ab611fbe7f254e59c1acded9
markusSMHI <44155634+markusSMHI@users.noreply.github.com>
2018-12-06 13:40:36 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -2,8 +2,8 @@
 utils::globalVariables(c("visit_year", "station_name", "sample_project_name_sv",
                          "sample_orderer_name_sv", "platform_code", "sample_date",
                          "sample_latitude_dd", "sample_longitude_dd", "positioning_system_code",
-                         "water_depth_m" 'Statistic', 'ymin', 'ymax', 'lower',
-                         'middle', 'upper', 'Value', 'Ok'))
+                         "water_depth_m", "Statistic", "ymin", "ymax", "lower",
+                         "middle", "upper", "Value", "Ok"))
 
 .onLoad <- function(libname, pkgname){
   clear_cache(age=36)
