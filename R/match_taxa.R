b48588e61b554eeb0cff6915e79861043f54d82d
markusSMHI <44155634+markusSMHI@users.noreply.github.com>
2018-12-11 11:43:52 +0100

    Update match_taxa.R

--- R/match_taxa.R
+++ R/match_taxa.R
@@ -1,4 +1,4 @@
-#' Taxon matching using WoRMS.
+#' Taxon matching using Dyntaxa
 #'
 #' @param names Vector of scientific names.
 #' @param ask Ask user in case of multiple matches.
@@ -54,7 +54,7 @@ match_taxa <- function(names, ask = TRUE) {
 
   for (i in seq_along(matches)) {
 
-    row <- list(scientificName = NA, scientificNameID = NA, match_type = NA)
+    row <- list(scientificName = NA, match_type = NA)
 
     match <- matches[[i]]
     if (is.data.frame(match)) {
@@ -63,25 +63,12 @@ match_taxa <- function(names, ask = TRUE) {
 
         # single match
 
-        row$scientificName = match$scientificname
-        row$scientificNameID = match$lsid
+        row$scientificName = match$scientific_name
         row$match_type = match$match_type
 
       } else if (ask) {
 
-        # multiple matches
-
-        print(match %>% select(AphiaID, scientificname, authority, status, match_type))
-        message(unames[i])
-        n <- readline(prompt = "Multiple matches, pick a number or leave empty to skip: ")
-        s <- as.integer(n)
-        if (!is.na(n) & n > 0 & n <= nrow(match)) {
-          row$scientificName = match$scientificname[s]
-          row$scientificNameID = match$lsid[s]
-          row$match_type = match$match_type[s]
         }
-
-      }
 
     }
 
