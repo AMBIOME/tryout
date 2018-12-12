83fb5be58657f157e61f374dcf11a84ed6d4892a
markusSMHI <44155634+markusSMHI@users.noreply.github.com>
2018-12-12 12:44:15 +0100

    Update check_datatype.R

--- R/check_datatype.R
+++ R/check_datatype.R
@@ -1,4 +1,4 @@
-#' Check if the required and recommended datatype-specific SHARK system fields (different between different datatypes) are present.
+#' Check if the required and recommended global and datatype-specific SHARK system fields (different between different datatypes) are present.
 #' 
 #' Missing or empty required fields are reported as errors,
 #' missing or empty recommended fields are reported as warnings.
@@ -9,6 +9,76 @@
 #' @export
 #' modified from https://iobis.github.io/obistools
 #' Provoost P and Bosch S (2018). “obistools: Tools for data enhancement and quality control.” Ocean Biogeographic Information System. Intergovernmental Oceanographic Commission of UNESCO. https://cran.r-project.org/package=obistools.
+
+check_datatype <- function(data, level = "error") {
+
+    errors <- data_frame()
+    required <- c("visit_year", "station_name", "sample_project_name_sv", "sample_orderer_name_sv", "platform_code", "sample_date", "sample_latitude_dd", "sample_longitude_dd", "positioning_system_code", "water_depth_m")
+    recommended <- c("monitoring_station_type_code", "monitoring_purpose_code", "monitoring_program_code", "reporting_institute_name_sv", "analytical_laboratory_name_sv")
+
+    # find missing required fields
+
+    fields <- missing_fields(data, required)
+    if (length(fields) > 0) {
+      errors <- bind_rows(errors, data_frame(
+        level = "error",
+        field = fields,
+        row = NA,
+        message = paste0("Required field ", fields, " is missing")
+      ))
+    }
+
+    # find empty values for required fields
+
+    for (field in required) {
+      if (field %in% names(data)) {
+        rows <- missing_values(data[,field])
+        if (length(which(rows)) > 0) {
+          errors <- bind_rows(errors, data_frame(
+            level = "error",
+            field = field,
+            row = which(rows),
+            message = paste0("Empty value for required field ", field)
+          ))
+        }
+      }
+    }
+
+    # recommended fields
+
+    if (level == "warning") {
+
+      # find missing recommended fields
+
+      fields <- missing_fields(data, recommended)
+      if (length(fields) > 0) {
+        errors <- bind_rows(errors, data_frame(
+          field = fields,
+          level = "warning",
+          message = paste0("Recommended field ", fields, " is missing")
+        ))
+      }
+
+      # find empty values for recommended fields
+
+      for (field in recommended) {
+        if (field %in% names(data)) {
+          rows <- missing_values(data[,field])
+          if (length(which(rows)) > 0) {
+            errors <- bind_rows(errors, data_frame(
+              level = "warning",
+              field = field,
+              row = which(rows),
+              message = paste0("Empty value for recommended field ", field)
+            ))
+          }
+        }
+      }
+
+    }
+
+    return(errors)
+}
 
 check_Bacterioplankton <- function(data, level = "error") {
 
